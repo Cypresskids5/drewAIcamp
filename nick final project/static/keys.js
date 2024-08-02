c587151dac5d6ad1fed2a48b90e94eb29754e975
Cypresskids5 <134014403+Cypresskids5@users.noreply.github.com>
2024-08-02 15:48:31 -0500

    Add files via upload

---
+++ nick final project/static/keys.js
@@ -0,0 +1,7 @@
+export const hugging_face_key = 'Bearer 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'
+export const cloudinaryCloudname = "dmm8zr0az"
+export const sightengine_api_user = "491630136"
+export const sightengine_api_secret = "C4MoxqQPspV7jVvMv728pcy2sTpjDe9A"
+export const sightengine_workflow = "wfl_g5fdiHz4maLRX7ezhd867"
+export const sightengine_listID = "tli_g5t6wWqDfOy6QW00gUBbQ"
+export const elevenlabs_api_key = "sk_7ab945bd743304205ed4e3a09101cc596c74cbe6cfea0eb6"
